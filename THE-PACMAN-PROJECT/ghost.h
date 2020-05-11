6e483ed6fec7ef162bd41905d57e192f74a85be6
Amy Elsayeh <amysamy@aucegypt.edu>
2020-05-11 18:21:18 +0200

    ghost.h

--- THE-PACMAN-PROJECT/ghost.h
+++ THE-PACMAN-PROJECT/ghost.h
@@ -1,5 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <queue>
+#include <fstream>
 using namespace sf;
 class ghost
 {
@@ -8,16 +9,23 @@ private:
 	Texture blinkyT, pinkyT, inkyT, clydeT;
 
 	int getOutScore[4];			//the score pac reaches when each ghost gets out of home
+
 public:
 
-	void BFS(int src);
-	
+	int AdjList[21][19];
+	int grid[21][19];
+	void matrix();
+
 	Sprite ghosts[4] = { blinky, pinky, inky, clyde };
 	Vector2i direction[4];
 	Vector2f startPosition[4];
 	int posIs[4];
 	int posJs[4];
 	ghost();
+
 	void setghosts();
 	void moveGhost(int bitmap[21][19]);
+	//vector<int>* AdjList;//vector
+	//void BFS(int src);
+	//void Graph(const int& p_Vertices);
 };
