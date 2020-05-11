2965c1f45cec03d1f88d10ceeacae6c22b8cc701
Shady Nessim <shady53@aucegypt.edu>
2020-05-11 17:52:16 +0200

    m

--- THE-PACMAN-PROJECT/Maze.cpp
+++ THE-PACMAN-PROJECT/Maze.cpp
@@ -29,7 +29,7 @@ Maze::Maze(string _BMF, string _TF, string _PF) {		//BMF bitmapfile, TF texture 
 				}
 				if (bitmap[i][j] == 2)
 				{
-					mazeSprites[i][j].scale(1.25f, 1.25f);
+					mazeSprites[i][j].scale(1.35f, 1.35f);
 					mazeSprites[i][j].setColor(Color::Red);
 				}
 
